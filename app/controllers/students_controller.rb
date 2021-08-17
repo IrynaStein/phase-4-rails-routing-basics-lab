c35033b874dab6d2e154935be5c45b3da2d92710
Iryna Stein <isophiestein@gmail.com>
2021-08-17 13:08:02 -0600

    commit tree e164be3bd0db9d411d0b6e664e2198641b4861d1

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,14 @@
+class StudentsController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        render json: Student.list_by_grades
+    end
+    
+    def highest_grade
+        render json: Student.list_by_grades.first
+    end
+end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,10 @@ class Student < ApplicationRecord
 
   def to_s
     "#{first_name} #{last_name}"
+  end 
+
+  def self.list_by_grades
+    self.order(grade: :desc)
   end
 
 end
